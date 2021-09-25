c48df386d4d69cbbee04906f429049ef43d6d491
hw <beewoo01@naver.com>
2021-09-25 16:29:45 +0900

    2021-09-22 회원가입, 계정정보 오류수정 완료, 이용약관 적용, 서버수정, 키보드, 데이터베이스 초기화, 그에 따른 오류 수정 완료

--- app/src/main/java/com/sdin/tourstamprally/ui/fragment/MainFragment.java
+++ app/src/main/java/com/sdin/tourstamprally/ui/fragment/MainFragment.java
@@ -79,16 +79,7 @@ public class MainFragment extends BaseFragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
 
-            // binding.rallyRecyclerview.addItemDecoration(new RallyRecyclerviewAdapterDeco(2, 50, true));
-
         }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        /*InputMethodManager inputMethodManager = (InputMethodManager) requireActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
-        inputMethodManager.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);*/
     }
 
     @Override
@@ -159,39 +150,6 @@ public class MainFragment extends BaseFragment {
         //binding.rallyRecyclerview.addItemDecoration(new RallyRecyclerviewAdapterDeco(2, 50, true));
     }
 
-    /*private class RallyRecyclerviewAdapterDeco extends RecyclerView.ItemDecoration{
-        private final int spanCount;
-        private final int spacing;
-        private final boolean includeEdge;
-
-        public RallyRecyclerviewAdapterDeco(int spanCount, int spacing, boolean includeEdge) {
-            this.spanCount = spanCount;
-            this.spacing = spacing;
-            this.includeEdge = includeEdge;
-        }
-
-        @Override
-        public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
-            int position = parent.getChildAdapterPosition(view); // item position
-            int column = position % spanCount; // item column
-
-            if (includeEdge) {
-                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
-                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
-
-                if (position < spanCount) { // top edge
-                    outRect.top = spacing;
-                }
-                outRect.bottom = spacing; // item bottom
-            } else {
-                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
-                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
-                if (position >= spanCount) {
-                    outRect.top = spacing; // item top
-                }
-            }
-        }
-    }*/
 
     private class RallyRecyclerviewAdapter extends RecyclerView.Adapter<RallyRecyclerviewAdapter.ViewHolder> {
 
@@ -267,9 +225,6 @@ public class MainFragment extends BaseFragment {
                 } else {
                     holder.binding.dibsImv.setImageResource(R.drawable.heart_resize);
                 }
-                /*if (adapterList.get(position).getAllPointCount() == adapterList.get(position).getMyHistoryCount()){
-                    holder.binding.dibsImv.setImageResource(R.drawable.full_heart_resize);
-                }*/
             }
 
 
@@ -359,145 +314,4 @@ public class MainFragment extends BaseFragment {
     }
 
 
-    /*private class RallyRecyclerviewAdapter extends RecyclerView.Adapter<RallyRecyclerviewAdapter.ViewHolder>{
-
-        private GuidDialog guidDialog;
-        private final Context context;
-        private final ArrayList<Tour_Spot> adapterList;
-        private final Map<Integer, Integer> progress_Map;
-        private final Map<Integer, Integer> history_Map;
-        private final ItemOnClick itemOnClick;
-
-
-        public RallyRecyclerviewAdapter(Context context, ArrayList<Tour_Spot> adapterList, Map<Integer, Integer> progress_Map, Map<Integer, Integer> history_Map){
-            this.context = context;
-            this.adapterList = adapterList;
-            this.progress_Map = progress_Map;
-            this.history_Map = history_Map;
-            this.itemOnClick = (ItemOnClick) requireActivity();
-        }
-
-        private int average(){
-            int sum = 0;
-            for (int i = 0; i < adapterList.size(); i++){
-                sum += adapterList.get(i).getTouristspot_checkin_count();
-            }
-
-            return sum / adapterList.size();
-        }
-
-        @NonNull
-        @Override
-        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return new ViewHolder(
-                    StepRallyLocationItemBinding.inflate(
-                            LayoutInflater.from(requireContext()),
-                            parent,
-                            false)
-            );
-        }
-
-        @SuppressLint("SetTextI18n")
-        @Override
-        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
-
-
-            holder.binding.location.setText(adapterList.get(position).getLocation_name());
-
-
-            if (!TextUtils.isEmpty(adapterList.get(position).getLocation_img())){
-                Log.wtf("이미지??", "???");
-                Glide.with(context).load("http://coratest.kr/imagefile/bsr/" + adapterList.get(position).getLocation_img())
-                        .error(ContextCompat.getDrawable(requireContext(), R.drawable.sample_bg))
-                        .into(new CustomTarget<Drawable>() {
-                    @Override
-                    public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
-                        holder.binding.mainLayout.setBackground(resource);
-
-                    }
-
-                    @Override
-                    public void onLoadCleared(@Nullable Drawable placeholder) {
-
-                    }
-                });
-            }
-
-            if (adapterList.get(position).getTouristspot_checkin_count() > average()) {
-                //list 내 CheckCount 의 평균값을 구한 뒤 평균 이상이면 HOT 띄우기
-                holder.binding.newsImv.setVisibility(View.VISIBLE);
-                Glide.with(holder.binding.newsImv.getContext()).load(R.drawable.hot_icon).into(holder.binding.newsImv);
-            }else if (((System.currentTimeMillis() - adapterList.get(position).getTouristspot_createtime()) / 10000 ) / (24 * 60 * 60) < 8){
-                //현재 시간과 관광지 등록시간이 7일 이하면 NEW 띄우기
-                holder.binding.newsImv.setVisibility(View.VISIBLE);
-                Glide.with(holder.binding.newsImv.getContext()).load(R.drawable.new_icon).into(holder.binding.newsImv);
-            }else {
-                holder.binding.newsImv.setVisibility(View.GONE);
-            }
-
-            if (progress_Map.get(adapterList.get(position).getLocation_idx()) != null
-                    &&  history_Map.get(adapterList.get(position).getLocation_idx()) != null) {
-
-
-                int allContents = progress_Map.get(adapterList.get(position).getLocation_idx());
-                int clearCount = history_Map.get(adapterList.get(position).getLocation_idx());
-
-                holder.binding.seekbar.setMax(allContents);
-                holder.binding.seekbar.setProgress(clearCount);
-                int allCountd = (int) ((double) clearCount /  (double) allContents * 100);
-                holder.binding.seekTxv.setText(allCountd + "%");
-
-            }else {
-                holder.binding.seekbar.setProgress(0);
-                holder.binding.seekTxv.setText(0 + "%");
-            }
-
-
-            Glide.with(holder.binding.dibsImv.getContext()).load(ContextCompat.getDrawable(requireContext()
-                    , Integer.parseInt(holder.binding.dibsImv.getTag().toString()) == 0?
-                             R.drawable.heart_resize : R.drawable.full_heart_resize))
-                    .into(holder.binding.dibsImv);
-
-            holder.binding.dibsImv.setOnClickListener( v -> {
-
-                if (Integer.parseInt(holder.binding.dibsImv.getTag().toString()) == 0) {
-                    holder.binding.dibsImv.setTag(1);
-                    Glide.with(holder.binding.dibsImv.getContext()).load(ContextCompat.getDrawable(requireContext(), R.drawable.full_heart_resize)).into(holder.binding.dibsImv);
-                } else {
-                    holder.binding.dibsImv.setTag(0);
-                    Glide.with(holder.binding.dibsImv.getContext()).load(ContextCompat.getDrawable(requireContext(), R.drawable.heart_resize)).into(holder.binding.dibsImv);
-                }
-
-            });
-
-
-
-        }
-
-        @Override
-        public int getItemCount() {
-            if (adapterList.size() > 4){
-                return 4;
-            }else {
-                return adapterList.size();
-            }
-
-        }
-
-
-
-        public class ViewHolder extends RecyclerView.ViewHolder{
-
-            public StepRallyLocationItemBinding binding;
-
-            public ViewHolder(@NonNull StepRallyLocationItemBinding binding) {
-                super(binding.getRoot());
-
-                this.binding = binding;
-
-                binding.stepRallyBg.setOnClickListener( v -> itemOnClick.onItemClick(adapterList.get(getAbsoluteAdapterPosition())));
-            }
-        }
-    }*/
 }
